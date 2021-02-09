8942449adfa4a97ed9c95f286495e0a44038daeb
KeziaAnnMathew <keziaann26@gmail.com>
2021-02-09 10:09:07 +0530

    backend commit

---
+++ model/userquery.js
@@ -0,0 +1,18 @@
+const mongoose =require("mongoose");
+mongoose.connect('mongodb+srv://userme:userme@ruhfiles.twknm.mongodb.net/areyouhealthy?retryWrites=true&w=majority');
+const Schema= mongoose.Schema;
+
+
+const QuerySchema = new Schema({
+   heading:String,
+   area:String,
+   comments:String,
+   suggestions:String,
+   userid:String,
+   img:String,
+   date:Date
+});
+
+var Querydata = mongoose.model('querydata', QuerySchema);
+
+module.exports = Querydata;
